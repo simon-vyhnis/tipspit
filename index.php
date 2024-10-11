d0c64dabc4df6cc8afc7c58bae0a40ce3526abca
Šimon Vyhnis <simon.vyhnis@gmail.com>
2024-10-11 08:30:41 +0200

    Add landing page

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="cz">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tipšpit</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="stylesheet" href="tipspit.css">
+</head>
+<body>
+    <div class="container">
+        <h1><i class="vlogo">v</i> Tipšpit</h1>
+        <p>Jak dopadne letošní florbalový turnaj? Tipni si a vyhraj! Stačí si u maturantů koupit vstupní kód a můžeš všem ukázat, že máš hru přečtenou.</p>
+        <form method="post">
+            <div class="mb-3">
+                <label for="entryCode" class="form-label">Vstupní kód</label>
+                <input type="text" class="form-control" id="entryCode" placeholder="AAA0000">
+            </div>
+            <button type="submit" class="btn btn-primary">Začít tipovat!</button>
+        </form>
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js" integrity="sha384-0pUGZvbkm6XF6gxjEnlmuGrJXVbNuzT9qBBavbLwCsOGabYfZo0T0to5eqruptLy" crossorigin="anonymous"></script>
+</body>
+</html>
